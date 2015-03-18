3ad3bbc7a710a81b184d0da0065b35400eeead30
kunou <t-kunou@esm.co.jp>
2015-03-19 00:15:50 +0900

    設定変更

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+  //db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || '192.168.33.10') + '/mean-dev',
   debug: true,
   //  aggregate: 'whatever that is not false, because boolean false value turns aggregation off', //false
   aggregate: false,
